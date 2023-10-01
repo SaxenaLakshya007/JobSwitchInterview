185f0e09edb73b66fa006fc3baced1b00aace5fc
sahilansari189 <121338629+sahilansari189@users.noreply.github.com>
2023-10-01 21:50:03 +0530

    Update gcd_n.cpp
    We validate user input for the number of numbers to ensure it's a positive integer.
    We use size_t for the loop variable in gcd_of_vector to prevent potential signed/unsigned comparison issues.

--- gcd_n.cpp
+++ gcd_n.cpp
@@ -1,33 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+
 typedef long long ll;
-ll gcd(ll a,ll b)
-{
-    if(a==0) return b; // base condition
-    ll d=gcd(b%a,a);
-    return d;
+
+ll gcd(ll a, ll b) {
+    if (a == 0) return b; // base condition
+    return gcd(b % a, a);
 }
-ll gcd_of_vector(vector <ll> &store)
-{
-    ll d=gcd(store[0],store[1]);
-    for(int i=2;i<store.size();i++)
-    {
-        d=gcd(d,store[i]);
+
+ll gcd_of_vector(const std::vector<ll> &store) {
+    ll d = store[0];
+    for (size_t i = 1; i < store.size(); i++) {
+        d = gcd(d, store[i]);
     }
     return d;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cout<<"How many numbers are there ? \n";
-    ll num; cin>>num;
-    cout<<"Enter your numbers\n";
-    vector <ll> store(num);
-    for(int i=0;i<num;i++)
-    {
-        cin>>store[i];
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    std::cout << "How many numbers are there? ";
+    ll num;
+    if (!(std::cin >> num) || num <= 0) {
+        std::cerr << "Invalid input. Please enter a positive integer for the number of numbers." << std::endl;
+        return 1; // Exit with an error code
     }
-    ll g_c_d=gcd_of_vector(store);
-    cout<<"GCD of these numbers is "<<g_c_d<<"\n";
+
+    std::cout << "Enter your numbers:\n";
+    std::vector<ll> store(num);
+    for (int i = 0; i < num; i++) {
+        std::cin >> store[i];
+    }
+
+    ll g_c_d = gcd_of_vector(store);
+    std::cout << "GCD of these numbers is " << g_c_d << "\n";
     return 0;
 }
